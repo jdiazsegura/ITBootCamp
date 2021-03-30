0e66d0287e8037f61c98f339bbe0d24f37ac22ad
Jose Armando Diaz Segura <jdiazseg9611@universidadean.edu.co>
2021-03-30 15:28:02 -0500

    Fix Controller Class name

--- Spring/calculadoracalorias/src/main/java/com/meli/calculadoracalorias/controllers/CalculateController.java
+++ Spring/calculadoracalorias/src/main/java/com/meli/calculadoracalorias/controllers/CalculateController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("calculate")
-public class CalculateService {
+public class CalculateController {
 
     @Autowired
     private CalculateServiceImpl calculateService;
